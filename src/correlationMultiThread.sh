d7efb99b18fe59fabfcf509957012f6a56c98ecd
neko2048 <show.999991@gmail.com>
2022-07-26 00:54:09 +0800

    log CS v.s. CG

--- src/correlationMultiThread.sh
+++ src/correlationMultiThread.sh
@@ -2,6 +2,6 @@ for hour in 1 3
 do
     for dBZ in $(seq 35 40)
     do
-        nohup echo $hour\ $dBZ | python correlation.py >> H$hour\D$dBZ.txt & 
+        nohup echo $hour\ $dBZ | python correlationLog.py >> H$hour\D$dBZ_log.txt & 
     done
 done
